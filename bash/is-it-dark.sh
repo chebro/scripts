277fbfa5285d94c3b577d44d6cd8081a32b002e6
Sravanth C <54331348+chebro@users.noreply.github.com>
2021-03-05 05:33:17 +0530

    add tuturu.sh

--- bash/is-it-dark.sh
+++ bash/is-it-dark.sh
@@ -2,10 +2,8 @@
 
 # Check if it's dark outside
 
-if [ $(date +%p) = AM ] && [ $(date +%H) -lt 7 ]; then
-    echo 1
-elif [ $(date +%p) = PM ] && [ $(date +%H) -gt 18 ]; then
-    echo 1
+if 	[[ "$(date +%p)" = AM && "$(date +%k)" -lt 7 ]] || [[ "$(date +%p)" = PM && "$(date +%k)" -gt 18 ]]; then
+   	echo 1
 else
     echo 0
 fi
---
+++ bash/tuturu.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+pixcat resize -w 64 -h 32 -W 512 -H 256 --align left ~/Pictures/mayushii.jpg
+echo tu, tu ruuu! UwU
